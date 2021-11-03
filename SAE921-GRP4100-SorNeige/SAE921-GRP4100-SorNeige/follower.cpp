bc2cfcc19768f2646b636f2be45dcb1c37c9fa37
Mylagos <71376015+Mylagos@users.noreply.github.com>
2021-11-03 16:24:51 +0100

    J'ai fait des trucs tkt

--- SAE921-GRP4100-SorNeige/SAE921-GRP4100-SorNeige/follower.cpp
+++ SAE921-GRP4100-SorNeige/SAE921-GRP4100-SorNeige/follower.cpp
@@ -1,1 +1,42 @@
 #include "follower.h"
+#include "equipment.h"
+void follower::setItemQty()
+{
+    for (const auto& i : inventory)
+    {
+        
+        i.get()->getTag()
+        
+        item ooo = *i;
+        if (ooo.getTag() == item::itemTag::weapon)
+        {
+            this->weaponQty++;
+        }
+        if (ooo.getTag() == item::itemTag::quest_item)
+        {
+            this->qItemQty++;
+        }
+        if (ooo.getTag() == item::itemTag::potion)
+        {
+            this->potionQty++;
+        }
+    }
+}
+
+void follower::printEquipment()
+{
+    int j=1;
+    for (const auto& i : inventory)
+    {
+        item ooo = *i;
+        equipment* eee = static_cast<equipment*>(i.get());
+        if (ooo.getTag() == item::itemTag::weapon)
+        {
+            std::cout << "[" << j << "]" << " " << ooo.getName() << ". ("
+        			<< eee->getStats().getStat(stats::name_stat::ability);
+            static_cast<equipment*>(i.get())->getStats().setStat(stats::name_stat::ability, 10);
+        }
+    }
+}
+
+
--- SAE921-GRP4100-SorNeige/SAE921-GRP4100-SorNeige/follower.h
+++ SAE921-GRP4100-SorNeige/SAE921-GRP4100-SorNeige/follower.h
@@ -13,7 +13,11 @@ private:
 
 	stats currentStat; //named currentstat to be easier to use in character
 
-	std::vector<item> inventory;
+	std::vector<std::unique_ptr<item>> inventory;
+
+	int weaponQty;
+	int qItemQty;
+	int potionQty;
 
 public:
 	void setName(std::string name_){this->name = name_;}
@@ -22,6 +26,11 @@ public:
 	void setDescription(std::string description_) { this->description = description_; }
 	std::string getDescription() { return description; }
 
-	void printInventory(); //Will be overridden in hero class
+	std::vector<std::unique_ptr<item>> getInventory() { return inventory; }
+
+	void setItemQty(); // Follower inventory. Will be overridden in hero class
+	void printEquipment();
+	void printInventory(); // Follower inventory. Will be overridden in hero class
+
 };
 
