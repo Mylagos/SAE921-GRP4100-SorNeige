495b547555b7a462be31f1fc10132015c8f937df
Mylagos <71376015+Mylagos@users.noreply.github.com>
2021-11-17 12:17:53 +0100

    Followercpp

--- SAE921-GRP4100-SorNeige/SAE921-GRP4100-SorNeige/follower.h
+++ SAE921-GRP4100-SorNeige/SAE921-GRP4100-SorNeige/follower.h
@@ -4,7 +4,8 @@
 #include <sstream>
 
 #include "Stats.h"
-#include "item.h"
+#include "Item.h"
+#include "Equipment.h"
 
 class Follower
 {
@@ -13,25 +14,30 @@ private:
 	std::string description_; // Follower description_ (Not used in hero)
 
 	Stats currentStat_; //named currentstat to be easier to use in character
+	Stats maxStats_;
 
-	std::vector<std::unique_ptr<item>> inventory_;
+	std::vector<std::unique_ptr<Item>> inventory_;
 
-	int weaponQty_;
-	int qItemQty_;
+	int gold_;
+	int provisions_;
+
 	int potionQty_;
+	int equipmentQty_;
+	int qItemQty_;
 
 public:
 	void setName(std::string name){this->name_ = name;}
-	std::string getName() { return name_; }
+	std::string getName() const { return name_; }
 
 	void setDescription(std::string description) { this->description_ = description; }
-	std::string getDescription() { return description_; }
+	std::string getDescription() const { return description_; }
 
-	std::vector<std::unique_ptr<item>> getInventory() { return inventory_; }
+	std::vector<std::unique_ptr<Item>> getInventory() const { return inventory_; }
 
 	void setItemQty(); // Follower inventory_. Will be overridden in hero class
-	std::stringstream printEquipment();
 	std::stringstream printInventory(); // Follower inventory_. Will be overridden in hero class
+	std::stringstream printEquipment();
+	std::stringstream printPotions();
 
 };
 
