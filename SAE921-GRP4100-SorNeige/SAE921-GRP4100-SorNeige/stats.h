4af9239abb575635fd7d0e3c1b1a187d073f1c57
BenjaminGonvers <gonversbenjamin@gmail.com>
2021-11-17 10:53:49 +0100

    Debug base

--- SAE921-GRP4100-SorNeige/SAE921-GRP4100-SorNeige/stats.h
+++ SAE921-GRP4100-SorNeige/SAE921-GRP4100-SorNeige/stats.h
@@ -4,7 +4,13 @@
 
 class Stats
 {
-
+public:
+enum class name_stat
+	{
+		ability = 0,
+		stamina,
+		luck
+	};
 private:
 	//main stat for all the game is here
 	int value_ability_ = 0;
@@ -16,21 +22,16 @@ private:
 	
 public:
 
-	enum class name_stat
-	{
-		ability = 0,
-		stamina,
-		luck
-	};
+	
 
 	Stats();//constructor that set all stat by default to 0
 	Stats(int, int, int);//constructor is for setting specific stat
 
-	int get_stat(name_stat);//get the value of the given Stats
-	void set_stat(name_stat, int);//set the value of the given Stats
-	int add_stat(name_stat, int);//add a value of the given Stats, can be positive or negative.
-	void add_all_stats(Stats);//add the value off the class Stats given
-	void sub_all_stats(Stats);//subtract the value off the class Stats given
+	int get_stat(name_stat);//get the value of the given stats_
+	void set_stat(name_stat, int);//set the value of the given stats_
+	int add_stat(name_stat, int);//add a value of the given stats_, can be positive or negative.
+	void add_all_stats(Stats);//add the value off the class stats_ given
+	void sub_all_stats(Stats);//subtract the value off the class stats_ given
 
 
 
